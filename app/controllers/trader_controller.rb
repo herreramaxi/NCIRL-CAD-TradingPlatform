619681b3a891a36b5017c2c84d0bf3c92b87a2cc
Maxi <herreramaxi@gmail.com>
2022-10-19 22:41:28 +0100

    Access to stock prices api from trader page.

--- app/controllers/trader_controller.rb
+++ app/controllers/trader_controller.rb
@@ -1,4 +1,23 @@
 class TraderController < ApplicationController
   def index
+    require 'uri'
+    require 'net/http'
+    require 'openssl'
+    
+    url = URI(ENV["STOCK_PRICES_API_URL"])
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["X-RapidAPI-Key"] = ENV["STOCK_PRICES_API_KEY"]
+    request["X-RapidAPI-Host"] = ENV["STOCK_PRICES_API_HOST"]
+
+    response = http.request(request)
+    body = response.read_body   
+    hash = JSON.parse(body)
+
+    @close= hash[hash.keys[0]]["Close"]
   end
 end
