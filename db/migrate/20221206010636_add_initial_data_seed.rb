7a96629a15f61d548b3df16ec90adcba0ee36dbd
Maxi <herreramaxi@gmail.com>
2022-12-06 03:06:28 +0000

    fix seed migration 2

--- db/migrate/20221206010636_add_initial_data_seed.rb
+++ db/migrate/20221206010636_add_initial_data_seed.rb
@@ -1,14 +1,14 @@
 class AddInitialDataSeed < ActiveRecord::Migration[7.0]
   def up
-    admin = Administrator.create(first_name: 'admin', last_name: 'admin', accountName: 'admin',
+    admin = Administrator.new(first_name: 'admin', last_name: 'admin', accountName: 'admin',
                                  email: 'admin@mhTrading.com', password: ENV['USER_PASSWORD'])
     admin.save
-    portfolioManager = PortfolioManager.create(first_name: 'portfolioManager', last_name: 'portfolioManagerLastName',
+    portfolioManager = PortfolioManager.new(first_name: 'portfolioManager', last_name: 'portfolioManagerLastName',
                                                accountName: 'portfolioManager', email: 'portfolioManager@mhTrading.com', password: ENV['USER_PASSWORD'])
     portfolioManager.save
 
     5.times do |i|
-      t = portfolioManager.traders.create(first_name: "trader#{i}", last_name: "lastName#{i}", accountName: "trader#{i}",
+      t = portfolioManager.traders.new(first_name: "trader#{i}", last_name: "lastName#{i}", accountName: "trader#{i}",
                                           email: "trader#{i}@mhTrading.com", password: ENV['USER_PASSWORD'], balance: 15_000)
       t.save
     end
@@ -21,12 +21,13 @@ class AddInitialDataSeed < ActiveRecord::Migration[7.0]
     csv_text = File.read(csvPath)
     csv = CSV.parse(csv_text, headers: true)
     csv.each do |row|
-      StockSymbol.create!(symbol: row.to_hash['Symbol'],
+      s= StockSymbol.new(symbol: row.to_hash['Symbol'],
                           name: row.to_hash['Name'],
                           country: row.to_hash['Country'],
                           ipo_year: row.to_hash['IPO YEAR'],
                           sector: row.to_hash['Sector'],
                           industry: row.to_hash['Industry'])
+      s.save                          
     end
   end
 
