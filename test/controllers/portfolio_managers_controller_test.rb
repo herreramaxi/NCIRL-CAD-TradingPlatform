aafb7d6d0a35f8b0b72b50329a10180df4701b57
Maxi <herreramaxi@gmail.com>
2022-12-24 00:48:16 +0000

    update portfolio managers controller tests

--- test/controllers/portfolio_managers_controller_test.rb
+++ test/controllers/portfolio_managers_controller_test.rb
@@ -20,7 +20,7 @@ class PortfolioManagersControllerTest < ActionDispatch::IntegrationTest
     sign_in_and_get_not_authrorized @trader, new_portfolio_manager_url
   end
 
-  test 'should create portfolio_manager' do
+  test 'should create portfolio_manager if signed-in is administrator' do
     sign_in_as @administrator
 
     assert_difference('PortfolioManager.count') do
@@ -32,6 +32,28 @@ class PortfolioManagersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to portfolio_managers_url
   end
 
+  test 'should not create portfolio_manager if signed-in is not administrator' do
+    sign_in_as @portfolio_manager
+
+    assert_no_difference('PortfolioManager.count') do
+      post portfolio_managers_url,
+           params: { portfolio_manager: { email: 'newEmail@local.com', first_name: 'newEmailFN', last_name: 'newEmailLN',
+                                          password: 'pass' } }
+    end
+
+    assert_redirected_to not_authorized_index_url
+
+    sign_in_as @trader
+
+    assert_no_difference('PortfolioManager.count') do
+      post portfolio_managers_url,
+           params: { portfolio_manager: { email: 'newEmail@local.com', first_name: 'newEmailFN', last_name: 'newEmailLN',
+                                          password: 'pass' } }
+    end
+
+    assert_redirected_to not_authorized_index_url
+  end
+
   test 'should show portfolio_manager' do
     sign_in_and_get_success @administrator, portfolio_manager_url(@portfolio_manager)
     sign_in_and_get_success @portfolio_manager, portfolio_manager_url(@portfolio_manager)
@@ -39,38 +61,42 @@ class PortfolioManagersControllerTest < ActionDispatch::IntegrationTest
     sign_in_and_get_not_authrorized @trader, portfolio_manager_url(@portfolio_manager)
   end
 
-    test "should get edit" do
-        sign_in_and_get_success @administrator, edit_portfolio_manager_url(@portfolio_manager)
-        sign_in_and_get_not_authrorized @portfolio_manager, edit_portfolio_manager_url(@portfolio_manager)
-        sign_in_and_get_not_authrorized @trader, edit_portfolio_manager_url(@portfolio_manager)
+  test 'should get edit' do
+    sign_in_and_get_success @administrator, edit_portfolio_manager_url(@portfolio_manager)
+    sign_in_and_get_not_authrorized @portfolio_manager, edit_portfolio_manager_url(@portfolio_manager)
+    sign_in_and_get_not_authrorized @trader, edit_portfolio_manager_url(@portfolio_manager)
+  end
+
+  test 'should update portfolio_manager if signed-in is administrator' do
+    updatedEmail = @portfolio_manager.email + 'modified'
+    updatedFirstName = @portfolio_manager.first_name + 'modified'
+    updatedLastName = @portfolio_manager.last_name + 'modified'
+    updatedPassword = 'password1234_modified'
+
+    assert_not_equal @portfolio_manager.authenticate('password1234'), false
+
+    sign_in_as @administrator
+
+    patch portfolio_manager_url(@portfolio_manager),
+          params: { portfolio_manager: { email: updatedEmail, first_name: updatedFirstName, last_name: updatedLastName,
+                                         password: updatedPassword } }
+    assert_redirected_to portfolio_managers_url
+
+    pmUpdated = PortfolioManager.find(@portfolio_manager.id)
+
+    assert_equal pmUpdated.email, updatedEmail
+    assert_equal pmUpdated.first_name, updatedFirstName
+    assert_equal pmUpdated.last_name, updatedLastName
+    assert_not_equal pmUpdated.authenticate(updatedPassword), false
+  end
+
+  test 'should destroy portfolio_manager' do
+    sign_in_as @administrator
+
+    assert_difference('PortfolioManager.count', -1) do
+      delete portfolio_manager_url(@portfolio_manager)
     end
 
-    test "should update portfolio_manager" do
-        # sign_in_and_method_success 
-        #     @administrator,
-        #     patch, 
-        #     portfolio_manager_url(@portfolio_manager), params: { portfolio_manager: { email: 'newEmail2@local.com', first_name: 'newEmailFN2', last_name: 'newEmailLN2',  password: 'pass2' } } }
-
-        updatedEmail= portfolio_manager.email + "2"
-        updatedFirstName= portfolio_manager.first_name + "2"
-        updatedLastName= portfolio_manager.last_name + "2"
-        updatedPassword=  "password1234_2"
-
-     sign_in_as @administrator
-
-      patch portfolio_manager_url(@portfolio_manager),  params: { portfolio_manager: { email: updatedEmail, first_name: updatedFirstName, last_name: updatedLastName,  password: updatedPassword } } 
-      assert_redirected_to portfolio_managers_url
-
-      pmUpdated = PortfolioManager.find(@portfolio_manager.id)
-
-      assert_equal pmUpdated.email = ""
-    end
-
-  #   test "should destroy portfolio_manager" do
-  #     assert_difference("PortfolioManager.count", -1) do
-  #       delete portfolio_manager_url(@portfolio_manager)
-  #     end
-
-  #     assert_redirected_to portfolio_managers_url
-  #   end
+    assert_redirected_to portfolio_managers_url
+  end
 end
